3e25745199823d2226464a235bba4ea3f3f19329
Owner <itamarharel@campus.technion.ac.il>
2021-06-16 15:38:53 +0300

    + beginning of StorageItem class

--- src/StorageItem.java
+++ src/StorageItem.java
@@ -1,3 +1,13 @@
-public class StorageItem {
+public abstract class StorageItem {
+    private String name;
+    private final int creationDate;
+    protected int size;
 
+    public StorageItem(String name) {
+        this.name = name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
